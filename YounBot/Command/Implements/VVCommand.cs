6059df15d2417cf6d504929063d5c2115832fcd6
Guimc <xiluo@guimc.ltd>
2025-03-28 17:48:28 +0800

    feat: update VvCommand to improve image retrieval and error handling

--- YounBot/Command/Implements/VVCommand.cs
+++ YounBot/Command/Implements/VVCommand.cs
@@ -12,10 +12,18 @@ public class VvCommand
     [Command("vv", "你能跟我看一辈子的vv吗")]
     public async Task Vv(BotContext context, MessageChain chain, string keyword)
     {
-        string url = $"https://vvsearch.top/api/mygo/img?keyword={UrlEncoder.Default.Encode(keyword)}";
+        Task<MessageResult> preMessage = context.SendMessage(MessageBuilder.Group(chain.GroupUin!.Value).Text("让我找找...").Build());
+        string url = $"https://api.zvv.quest/search?q={UrlEncoder.Default.Encode(keyword)}&n=1";
         JsonObject response = await HttpUtils.GetJsonObject(url);
-        string vvUrl = response["urls"][0]["url"].GetValue<string>();
+        if (response["code"].GetValue<int>() != 200)
+        {
+            await context.SendMessage(MessageBuilder.Group(chain.GroupUin.Value).Text("找不到...").Build());
+            return;
+        }
+        string vvUrl = response["data"][0].GetValue<string>();
         byte[] image = await HttpUtils.GetBytes(vvUrl);
+        preMessage.Wait();
+        await context.RecallGroupMessage(chain.GroupUin!.Value, preMessage.Result);
         await context.SendMessage(MessageBuilder.Group(chain.GroupUin.Value).Image(image).Build());
     }
 }
