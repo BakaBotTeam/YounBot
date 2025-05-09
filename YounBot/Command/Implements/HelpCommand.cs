d3b450ec3d379487d7fa610b089931c0e2df6c33
Guimc <xiluo@guimc.ltd>
2025-05-09 17:15:05 +0800

    feat: wrap help command result into multimsg

--- YounBot/Command/Implements/HelpCommand.cs
+++ YounBot/Command/Implements/HelpCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Lagrange.Core;
+using Lagrange.Core.Common.Interface.Api;
 using Lagrange.Core.Message;
-using static YounBot.Utils.MessageUtils;
+
 namespace YounBot.Command.Implements;
 
 public class HelpCommand
@@ -8,7 +9,11 @@ public class HelpCommand
     [Command("help", "列出所有指令")]
     public async Task Help(BotContext context, MessageChain chain)
     {
-        await SendMessage(context, chain, CommandManager.Instance.Descriptions.Select(command => $"{CommandManager.GetCommandPrefix()}{command.Key} {CommandManager.Instance.GetCommandArgs(command.Key)}: {command.Value}").Aggregate((current, next) => $"{current}\n{next}"));
+        await context.SendMessage(MessageBuilder.Group(chain.GroupUin!.Value).MultiMsg(MessageBuilder.Friend(10000)
+            .Text(CommandManager.Instance.Descriptions
+                .Select(command =>
+                    $"{CommandManager.GetCommandPrefix()}{command.Key} {CommandManager.Instance.GetCommandArgs(command.Key)}: {command.Value}")
+                .Aggregate((current, next) => $"{current}\n{next}")).Build()).Build());
     }
     
 }
